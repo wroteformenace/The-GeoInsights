926d33e462e784656ab56c24f4657dbb368085fe
wroteformenace <richardfineman083@gmail.com>
2025-06-29 01:09:42 +0530

    added about.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'; // ✅ Import added
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Videos from './pages/Videos';
+import About from './pages/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />          {/* ✅ Default route */}
         <Route path="/videos" element={<Videos />} />  {/* ✅ Videos route */}
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </>
   );
