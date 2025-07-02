95a809fbdd1e66946079e94c246fa659e55fcf8e
wroteformenace <richardfineman083@gmail.com>
2025-07-02 13:06:57 +0530

    vite rollupoptions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-  plugins: [react(), svgr()],
-});
+export default {
+  build: {
+    rollupOptions: {
+      external: ['your-external-package']
+    }
+  }
+}
